af1bba99ef54fbd088a92f9ab149791262ccf17e
carolina <cstapiarubio@gmail.com>
2021-07-02 22:51:05 -0400

    listo animaciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+//menu toggle
 $( ".menu-toggler" ).click(function () {
     $(".menu_nav").toggle();
   });
+
+//mostrar lista de nav en desktop
+/*$(".menu_nav").show(function () {
+  if 
+  $(window).width("1200");
+});*/
